00e37b4c12b8391852e9a9c82bae9aff420406dd
Rajpreet0 <rajpreet.singh@gmx.de>
2025-07-01 20:43:27 +0200

    added projects page

---
+++ src/app/projects/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailProjectPage = () => {
+  return (
+    <div className='text-white text-xl'>DetailProjectPage</div>
+  )
+}
+
+export default DetailProjectPage
